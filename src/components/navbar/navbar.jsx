3499baabb0821c857d7dcf1e058fc6385b189603
Charger1256A <aditya.ram112005@gmail.com>
2021-11-07 12:26:06 -0800

    finished setting up redux and used it for modal

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,6 +4,8 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import logo from "../../assets/logo.png";
 import { makeStyles } from "@material-ui/core/styles";
+import { useSelector, useDispatch } from 'react-redux'
+import { open } from '../../features/modal/modalSlice.js'
 import './navbar.css';
 
 const useStyles = makeStyles((theme) => ({
@@ -20,6 +22,8 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Navbar({ user, onLogout }) {
+  const modal = useSelector((state) => state.modal.value);
+  const dispatch = useDispatch();
   const classes = useStyles();
 
   const logout = () => {
@@ -48,7 +52,7 @@ export default function Navbar({ user, onLogout }) {
         <Typography variant="h6" className={classes.title}>
           Paly Robotics
         </Typography>
-        <Button className="navbar-button">+</Button>
+        <Button className="navbar-button" onClick={() => dispatch(open())}>+</Button>
         <Button color="inherit" variant="outlined" onClick={() => logout()}>LOGOUT</Button>
       </Toolbar>
       }
--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -1,7 +1,38 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux'
+import { open, close } from '../../features/modal/modalSlice.js'
+import { Modal } from "antd";
+
 export default function Home() {
+    const modal = useSelector((state) => state.modal.value);
+    const dispatch = useDispatch();
+
+
+    const showModal = () => {
+        dispatch(open());
+      };
+    
+      const handleOk = () => {
+        dispatch(close());
+      };
+    
+      const handleCancel = () => {
+        dispatch(close());
+      };
+    
     return (
         <div>
-            <h1>Home</h1>
+            <Modal
+            title="Basic Modal"
+            visible={modal}
+            onOk={handleOk}
+            onCancel={handleCancel}
+            okText="Add"
+            >
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
         </div>
-    )
+  );
 }
