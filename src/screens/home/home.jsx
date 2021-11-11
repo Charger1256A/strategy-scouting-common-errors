a0310dc760951002c769d16dfe8c56cf25928a36
Charger1256A <aditya.ram112005@gmail.com>
2021-11-11 12:57:17 -0800

    worked on column & row styling

--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -1,13 +1,60 @@
 import { useState, useEffect } from "react";
 import BugModal from '../../components/bugModal/bugModal';
 import ProjectModal from '../../components/projectModal/projectModal';
+import BugCard from '../../components/bugCard/bugCard';
+import { db } from "../../firebase";
+import { Row, Col } from 'antd';
+import './home.css';
 
 export default function Home() {
+    const [bugs, setBugs] = useState();
+    const [localProjects, setLocalProjects] = useState({});
+
+    const projects = db.ref("/projects");
+
+    useEffect(() => {
+        projects.on("value", function (snapshot) {
+            setLocalProjects(snapshot.val());
+            console.log(snapshot.val());
+          });
+    }, [])
+
+    const display = () => {
+        const rows = [];
+        let cols = [];
+        var i = 0;
+        Object.keys(localProjects).map((project, key) => (
+            Object.keys(localProjects[project].bugs).map((bug, key) => {
+                cols.push(<BugCard className="bug-card" title={localProjects[project].bugs[bug].title}/>)
+                if ((i + 1) % 4 === 0) {
+                    rows.push(cols);
+                    cols = [];
+                }
+                i++
+            })
+        ))
+        if (cols && cols.length > 0) {
+            rows.push(cols);
+        }
+        console.log(rows);
+        return rows;
+    }
     
     return (
-        <div>
+        <div style={{ margin: "auto", width: "80%", padding: 10 }}>
             <ProjectModal />
             <BugModal />
+            {/* <div className="container">            {
+                Object.keys(localProjects).map((project, key) => (
+                    Object.keys(localProjects[project].bugs).map((bug, key) => (
+                        <BugCard className="bug-card" title={localProjects[project].bugs[bug].title}/>
+                    ))
+                ))
+            } 
+            </div>  */}
+            <div>
+            {display().map((i, k) => i && i.length > 0 && <Row style={{marginBottom: 10}} key={`row-${k}`}>{i.map((c, ck) => <Col span={6} key={`key-${ck}`}>{c}</Col>)}<br/></Row>)}
+            </div>
         </div>
   );
 }
